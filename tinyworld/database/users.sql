4cedb48f2b1f8c24777f7ce6fda9a3cb6a61a703
vagrant <vagrant@precise32.(none)>
2014-04-20 16:45:18 +0000

    added postgres database

---
+++ tinyworld/database/users.sql
@@ -0,0 +1,51 @@
+drop table if exists users cascade;
+drop table if exists posts cascade;
+drop table if exists relations cascade;
+
+create table users (
+    uid SERIAL,
+    username varchar(50),
+    password varchar(25),
+    online varchar(10),
+    primary key (uid)
+);
+
+create table posts (
+	pid SERIAL,
+	username varchar(50),
+	post varchar(300),
+	primary key (pid)
+);
+
+create table relations (
+	uid int,
+	pid int,
+	foreign key (uid) references users,
+	foreign key (pid) references posts,
+	unique(uid,pid)
+);
+
+/*
+insert into users (fname, lname, password, age) values ('John', 'Doe', 'xxxx', 27);
+insert into users (fname, lname, password, age) values ('Jane', 'Doe', 'yyyy', 28);
+insert into users (fname, lname, password, age) values ('Bill', 'Flood', 'aaaa', 29);
+insert into users (fname, lname, password, age) values ('Veb', 'Nordhagen', 'bbbb', 30);
+insert into users (fname, lname, password, age) values ('Hazel', 'Nutting', 'cccc', 4);
+insert into users (fname, lname, password, age) values ('Caleb', 'Manu', 'dddd', 7);
+insert into users (fname, lname, password, age) values ('Aiden', 'Hall', 'eeee', 19);
+*/
+--insert into address values (1, '1 mallard drive', 'cambridge', 'MA', '34567');
+--insert into address values (2, '21 jump street', 'new york', 'NY', '98765');
+--insert into address values (3, '4 cherry lane', 'truffala', 'NJ', '58235');
+--insert into address values (4, '16 strong road', 'chutney', 'VT', '38573');
+--insert into address values (5, '99 livingston circle', 'amherst', 'MA', '99822');
+--insert into address values (6, '1123 main street', 'worcester', 'MA', '22234');
+
+--insert into lives values
+--	(1, 6),
+--	(2, 4),
+--	(3, 6),
+--	(4, 2),
+--	(5, 5),
+--	(6, 1),
+--	(7, 3);
--- tinyworld/routes/index.js
+++ tinyworld/routes/index.js
@@ -1,12 +1,6 @@
 
-/*
- * GET home page.
- */
 
 exports.index = function(req, res){
   res.render('index', { title: 'Welcome to Tiny World' });
 };
 
-exports.getProfiles = function (req, res) {
-    res.render('profiles', {title: 'TinyWorld'});
-};
